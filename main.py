2445f6dc6d9a74e383deed2b931aa288b0fc7eee
Minh Quan Do <mdo6180@gmail.com>
2023-12-04 16:27:26 -0500

    added example of html inside node

--- main.py
+++ main.py
@@ -46,3 +46,34 @@ def d3():
         ]
     }
     return render_template("d3.html", json_data=json_data)
+
+
+@app.route("/d3_practice")
+def d3_practice():
+    json_data = {
+        "nodes": [
+            {"id": 1, "name": "A", "label": "<h3>Node A</h3><p>HTML content for Node A</p>"},
+            {"id": 2, "name": "B", "label": "<h3>Node B</h3><p>HTML content for Node B</p>"},
+            {"id": 3, "name": "C", "label": "<h3>Node C</h3><p>HTML content for Node C</p>"},
+            {"id": 4, "name": "D", "label": "<h3>Node D</h3><p>HTML content for Node D</p>"},
+            {"id": 5, "name": "E", "label": "<h3>Node E</h3><p>HTML content for Node E</p>"},
+            {"id": 6, "name": "F", "label": "<h3>Node F</h3><p>HTML content for Node F</p>"},
+            {"id": 7, "name": "G", "label": "<h3>Node G</h3><p>HTML content for Node G</p>"},
+            {"id": 8, "name": "H", "label": "<h3>Node H</h3><p>HTML content for Node H</p>"},
+            {"id": 9, "name": "I", "label": "<h3>Node I</h3><p>HTML content for Node I</p>"},
+            {"id": 10, "name": "J", "label": "<h3>Node J</h3><p>HTML content for Node J</p>"}
+        ],
+        "links": [
+            {"source": 1, "target": 2},
+            {"source": 1, "target": 5},
+            {"source": 1, "target": 6},
+            {"source": 2, "target": 3},
+            {"source": 2, "target": 7},
+            {"source": 3, "target": 4},
+            {"source": 8, "target": 3},
+            {"source": 4, "target": 5},
+            {"source": 4, "target": 9},
+            {"source": 5, "target": 10},
+        ]
+    } 
+    return render_template("d3_practice.html", json_data=json_data)
