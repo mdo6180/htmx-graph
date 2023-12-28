c83cfe5f1f6dc6bb371804b26bb66a38b1305791
Minh Quan Do <mdo6180@gmail.com>
2023-12-28 02:12:09 -0500

    D3 response now works with FastAPI

--- main.py
+++ main.py
@@ -29,3 +29,94 @@ async def index(request: Request):
 @app.get("/evaluation", response_class=HTMLResponse)
 async def evaluation():
     return "<h1>evaluation</h1>"
+
+@app.get("/d3", response_class=HTMLResponse)
+async def d3(request: Request):
+    json_data = {
+        "nodes": [
+            {
+                "id": 1,
+                "name": "A"
+            },
+            {
+                "id": 2,
+                "name": "B"
+            },
+            {
+                "id": 3,
+                "name": "C"
+            },
+            {
+                "id": 4,
+                "name": "D"
+            },
+            {
+                "id": 5,
+                "name": "E"
+            },
+            {
+                "id": 6,
+                "name": "F"
+            },
+            {
+                "id": 7,
+                "name": "G"
+            },
+            {
+                "id": 8,
+                "name": "H"
+            },
+            {
+                "id": 9,
+                "name": "I"
+            },
+            {
+                "id": 10,
+                "name": "J"
+            }
+        ],
+        "links": [
+            {
+                "source": 1,
+                "target": 2
+            },
+            {
+                "source": 1,
+                "target": 5
+            },
+            {
+                "source": 1,
+                "target": 6
+            },
+
+            {
+                "source": 2,
+                "target": 3
+            },
+            {
+                "source": 2,
+                "target": 7
+            },
+            {
+                "source": 3,
+                "target": 4
+            },
+            {
+                "source": 8,
+                "target": 3
+            },
+            {
+                "source": 4,
+                "target": 5
+            },
+            {
+                "source": 4,
+                "target": 9
+            },
+            {
+                "source": 5,
+                "target": 10
+            }
+        ]
+    }
+    return templates.TemplateResponse("d3_practice.html", {"request": request, "json_data": json_data, "title": "D3 Practice"})
