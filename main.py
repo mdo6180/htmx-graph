a86497e9881039641b7cd0bea03dead05a47f5fd
Minh Quan Do <mdo6180@gmail.com>
2024-01-04 15:43:01 -0500

    added htmx polling to provide status updates

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-from fastapi import FastAPI, APIRouter
+import random
+from typing import Annotated
+
+from fastapi import FastAPI, APIRouter, Header
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
@@ -18,12 +21,12 @@ app.mount("/app2", app2)
 
 json_data = {
     "nodes": [
-        { "id": "kspacey", "label": "Kevin Spacey", "width": 144, "height": 100, "endpoint": "/node/kspacey" },
-        { "id": "swilliams", "label": "Saul Williams", "width": 160, "height": 100, "endpoint": "/node/swilliams" },
-        { "id": "bpitt", "label": "Brad Pitt", "width": 108, "height": 100, "endpoint": "/node/bpitt" },
-        { "id": "hford", "label": "Harrison Ford", "width": 168, "height": 100, "endpoint": "/node/hford" },
-        { "id": "lwilson", "label": "Luke Wilson", "width": 144, "height": 100, "endpoint": "/node/lwilson" },
-        { "id": "kbacon", "label": "Kevin Bacon", "width": 121, "height": 100, "endpoint": "/node/kbacon" }
+        { "id": "kspacey", "label": "Kevin Spacey", "width": 144, "height": 100, "endpoint": "/node/kspacey", "progress_endpoint": "/progress/kspacey" },
+        { "id": "swilliams", "label": "Saul Williams", "width": 160, "height": 100, "endpoint": "/node/swilliams", "progress_endpoint": "/progress/swilliams" },
+        { "id": "bpitt", "label": "Brad Pitt", "width": 108, "height": 100, "endpoint": "/node/bpitt", "progress_endpoint": "/progress/bpitt" },
+        { "id": "hford", "label": "Harrison Ford", "width": 168, "height": 100, "endpoint": "/node/hford", "progress_endpoint": "/progress/hford" },
+        { "id": "lwilson", "label": "Luke Wilson", "width": 144, "height": 100, "endpoint": "/node/lwilson", "progress_endpoint": "/progress/lwilson" },
+        { "id": "kbacon", "label": "Kevin Bacon", "width": 121, "height": 100, "endpoint": "/node/kbacon", "progress_endpoint": "/progress/kbacon" }
     ],
     "edges": [
         {"source": "kspacey", "target": "swilliams", "arrowhead": "vee" },
@@ -57,3 +60,26 @@ async def insert(request: Request, node_id: str):
             return f"""<p> node clicked: {node["label"]} </p>"""
         
     return "<p> node does not exist </p>"
+
+
+progress_dict = {
+    "kspacey": 0,
+    "swilliams": 0,
+    "bpitt": 0,
+    "hford": 0,
+    "lwilson": 0,
+    "kbacon": 0
+}
+
+@app.get("/progress/{node_id}", response_class=HTMLResponse)
+async def progress(request: Request, node_id: str, response: Response):
+    global progress_dict
+
+    added_progress = random.randint(1, 5)
+    if progress_dict[node_id] + added_progress < 100:
+        progress_dict[node_id] += added_progress
+        return f"{progress_dict[node_id]}%"
+    elif progress_dict[node_id] + added_progress >= 100:
+        progress_dict[node_id] = 100
+        response.headers["HX-Trigger"] = "done"
+        return "100%"
